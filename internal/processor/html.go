b5f4ee07ad91158f6b5e8e89fb00bc220a21cae7
Sbbke <fghjklllll@gmail.com>
2026-06-13 14:17:55 -0500

    parsing html to get raw text

--- internal/processor/html.go
+++ internal/processor/html.go
@@ -4,6 +4,9 @@ import (
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 	"net/http"
+	"bytes"
+	"regexp"
+	"strings"
 	"fmt"
 )
 
@@ -43,4 +46,44 @@ func (hp *HtmlProcessor) Run(url string) error{
 	}
 
 	return nil
+}
+
+func CleanHTML(rawHTML string) (string, error) {
+	doc, err := html.Parse(strings.NewReader(rawHTML))
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	
+	// Define a recursive function to traverse the HTML node tree
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		// If the current node is a script, style, nav, svg, etc., skip parsing its children
+		if n.Type == html.ElementNode {
+			tag := strings.ToLower(n.Data)
+			if tag == "script" || tag == "style" || tag == "svg" || tag == "nav" || tag == "header" || tag == "footer" || tag == "noscript" {
+				return 
+			}
+		}
+
+		// If it's a plain text node, append its content
+		if n.Type == html.TextNode {
+			buf.WriteString(n.Data)
+			buf.WriteString(" ")
+		}
+
+		// Keep traversing down the tree
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	
+	f(doc)
+
+	// Regex to collapse multiple spaces, tabs, and newlines down to a single space
+	re := regexp.MustCompile(`\s+`)
+	cleanText := re.ReplaceAllString(buf.String(), " ")
+
+	return strings.TrimSpace(cleanText), nil
 }
