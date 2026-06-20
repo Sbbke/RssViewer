31864a0a0af44a7b42e635666d790cb9cdf68ee8
Sbbke <fghjklllll@gmail.com>
2026-06-20 14:34:10 -0500

    sql writer implementation

--- internal/storage/writer/sql.go
+++ internal/storage/writer/sql.go
@@ -1,12 +1,35 @@
 package writer
 
+import (
+	"database/sql"
+	"RssViewer/internal/db"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 type DBWriter struct {
+	db *sql.DB
 }
 
-func (w *DBWriter) Create() error {
+func NewDBwriter(db *sql.DB) (*DBWriter, error) {
+	return &DBWriter{db: db}, nil
+}
+
+func (w *DBWriter) CreateTopic(model *db.TopicModel) error {
+	
+	return nil
+}
+
+func (w *DBWriter) CreateRss(model *db.RSSModel) error {
 
 	return nil
 }
+
+func (w *DBWriter) CreatePost(model *db.PostModel) error {
+
+	return nil
+}
+
+
 
 func (w *DBWriter) Update() error {
 	return nil
