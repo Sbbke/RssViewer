8b7e92d83e9d4093ba957d8ccb5c856744bbc201
Sbbke <fghjklllll@gmail.com>
2026-07-25 09:18:39 +0800

    add rss service controller

--- app.go
+++ app.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-
 )
 
 // App struct
@@ -16,6 +15,7 @@ type App struct {
 	ctx          context.Context
 	orch         *storage.DataOrch
 	topicService *service.TopicService
+	rssService *service.RssService
 }
 
 // NewApp creates a new App application struct
@@ -29,6 +29,7 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 	a.orch = setupOrch()
 	a.topicService = service.NewTopicService(a.orch)
+	a.rssService = service.NewRssService(a.orch)
 }
 
 // Greet returns a greeting for the given name
@@ -60,6 +61,20 @@ func (a *App) DeleteTopic(id int64) error {
 	return a.topicService.DeleteTopic(id)
 }
 
+// SubmitRssUrl fetches, parses, and persists an RSS feed from the given URL.
+func (a *App) SubmitRssUrl(rssURL string) (dto.RssItem, error) {
+	return a.rssService.SubmitRssUrl(a.ctx, rssURL)
+}
+
+// CheckRssUpdate checks all subscribed feeds for new posts.
+func (a *App) CheckRssUpdate() error {
+	return a.rssService.CheckUpdate()
+}
+
+// RemoveRss deletes an RSS feed by ID.
+func (a *App) RemoveRss(id int64) error {
+	return a.rssService.RemoveRss(id)
+}
 func setupOrch() *storage.DataOrch {
 	if err := os.MkdirAll("temp", 0755); err != nil {
 		log.Fatalf("error creating temp dir: %v", err)
